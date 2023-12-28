8bacb1f4e3a72a133e1d9b6b817ab963e03771be
junpei ogawa <cornerstone.am3@gmail.com>
2023-12-28 17:18:25 +0900

    setup vite-plugin-checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: true
+    })
+  ],
   resolve: {
     alias: [{ find: '@styles', replacement: '/src/styles' }]
   },
