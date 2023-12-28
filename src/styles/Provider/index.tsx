b1b5328154b897667f6f0e46538a5e0270a49290
junpei ogawa <cornerstone.am3@gmail.com>
2023-12-28 22:01:07 +0900

    setup mui

--- src/styles/Provider/index.tsx
+++ src/styles/Provider/index.tsx
@@ -1,10 +1,22 @@
 import { ThemeProvider as EmotionThemeProvider } from '@emotion/react'
+import { createTheme } from '@mui/material/styles'
 import { FC, useMemo } from 'react'
+
 import { colors, styles } from '../theme'
 import { ThemeProviderProps } from './types'
 
 export const ThemeProvider: FC<ThemeProviderProps> = ({ children }) => {
   const theme = useMemo(() => ({ colors, styles }), [])
 
-  return <EmotionThemeProvider theme={theme}>{children}</EmotionThemeProvider>
+  const muiTheme = useMemo(() => {
+    return createTheme({
+      palette: { primary: colors.app.primary }
+    })
+  }, [])
+
+  return (
+    <EmotionThemeProvider theme={muiTheme}>
+      <EmotionThemeProvider theme={theme}>{children}</EmotionThemeProvider>
+    </EmotionThemeProvider>
+  )
 }
--- src/styles/emotion.d.ts
+++ src/styles/emotion.d.ts
@@ -1,8 +1,10 @@
 import '@emotion/react'
+import { Theme as MUITheme } from '@mui/material/styles'
+
 import { AppColors, AppStyles } from './theme'
 
 declare module '@emotion/react' {
-  export interface Theme {
+  export interface Theme extends MUITheme {
     colors: AppColors
     styles: AppStyles
   }
