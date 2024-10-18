a0aacfdc4fc56b9a63afe00ce38bfb5bf56684f0
junpei ogawa <cornerstone.am3@gmail.com>
2024-10-18 19:21:42 +0900

    fix type error

--- src/components/commons/Popover/index.tsx
+++ src/components/commons/Popover/index.tsx
@@ -1,7 +1,7 @@
 import MUIPopover from '@mui/material/Popover'
-import { type FC, forwardRef } from 'react'
+import { forwardRef } from 'react'
 import type { PopoverProps } from './types'
 
-export const Popover: FC<PopoverProps> = forwardRef<HTMLDivElement, PopoverProps>((props, ref) => {
+export const Popover = forwardRef<HTMLDivElement, PopoverProps>((props, ref) => {
   return <MUIPopover {...props} ref={ref} />
 })
